3b37ea3ea39ac80bb70ce66415bc0a25dd5792ae
yadro <yadrone@gmail.com>
2016-06-09 17:37:26 +0300

    add word

--- src/components/printEquation.tsx
+++ src/components/printEquation.tsx
@@ -7,9 +7,21 @@ import {FractionType} from "../helper/fraction.js";
 interface Equaltion {
     x?: number;
     sign?: number;
+    word?: string;
     fraction?: FractionType;
 }
+
+const signs = {
+    '-1': ' - ',
+    0: ' = ',
+    1: ' + ',
+    2: '&middot;',
+    3: ' : ',
+}
 export default class PrintEquationComp extends React.Component{
+    /**
+     * @example x1 = + 1/2·x1 - 1/2·x2 - 1/2·x3 + 1/2·x4 + 5(-1/2·x4 + 10)
+     */
     equation: Equaltion[] = [
         {x: 1},
         {sign: 0},
@@ -27,10 +39,21 @@ export default class PrintEquationComp extends React.Component{
         {sign: -1},
         {fraction: new Fraction(-1,2)},
         {x: 4},
+
+        {sign: 1},
+        {fraction: new Fraction(5)},
+        {sign: 2},
+        {word: '('},
+        {fraction: new Fraction(-1,2)},
+        {x: 4},
+        {sign: 1},
+        {fraction: new Fraction(10)},
+        {word: ')'},
     ];
     
     constructor(props) {
         super(props);
+        this.equation = props.equation || this.equation;
     }
 
     sub(value) {
@@ -40,24 +63,14 @@ export default class PrintEquationComp extends React.Component{
     render() {
         let buf = '';
         let last: Equaltion = {};
-        this.equation.forEach((el: any, idx) => {
+        this.equation.forEach((el: Equaltion, idx) => {
             if (el.hasOwnProperty('x')) {
                 if (last.hasOwnProperty('fraction')) {
                     buf += '&middot;';
                 }
                 buf += 'x' + this.sub(el.x);
             } else if (el.hasOwnProperty('sign')) {
-                switch (el.sign) {
-                    case 0:
-                        buf += ' = ';
-                        break;
-                    case 1:
-                        buf += ' + ';
-                        break;
-                    case -1:
-                        buf += ' - ';
-                        break;
-                }
+                buf += signs[el.sign];
             } else if (el.hasOwnProperty('fraction')) {
                 let fraction = el.fraction;
                 if (last.hasOwnProperty('sign') && last.sign !== 0) {
@@ -76,6 +89,8 @@ export default class PrintEquationComp extends React.Component{
                     }
                 }
                 buf += fraction.toFraction();
+            } else if (el.hasOwnProperty('word')) {
+                buf += el.word;
             } else {
                 buf += '{???}';
             }
@@ -101,8 +116,12 @@ class PrintEquation extends React.Component{
         this.equation.push({x: num});
     }
     
-    pushWord(sign) {
+    pushSign(sign) {
         this.equation.push({sign});
+    }
+
+    pushWord(word) {
+        this.equation.push({word});
     }
     
     pushFraction(fraction) {
